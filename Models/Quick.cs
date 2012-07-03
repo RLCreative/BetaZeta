994c48eca016da894806d6bd14cd33a81fb3646d
Allen Bourne <allen@regallager.com>
2012-07-03 18:25:20 -0400

    Error Message Fix

--- Models/Quick.cs
+++ Models/Quick.cs
@@ -14,22 +14,22 @@ namespace Zeta.Models
         //strRegex = strRegex + "@\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\";
         //strRegex = strRegex + "@.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
 
-        [Required(ErrorMessage = "Required")]
-        [StringLength(50, MinimumLength = 3, ErrorMessage = "Must have a minimum length of 3.")]
+        [Required(ErrorMessage = "Please enter a First Name")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Please enter a First Name")]
         public string FirstName { get; set; }
 
-        [Required(ErrorMessage = "Required")]
-        [StringLength(50, MinimumLength = 3, ErrorMessage = "Must have a minimum length of 3.")]
+        [Required(ErrorMessage = "Please enter a Last Name")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Please enter a Last Name")]
         public string LastName { get; set; }
 
         [StringLength(12)]
-        [Required(ErrorMessage = "Required")]
-        [RegularExpression(@"^\s*([\(]?)\[?\s*\d{3}\s*\]?[\)]?\s*[\-]?[\.]?\s*\d{3}\s*[\-]?[\.]?\s*\d{4}$", ErrorMessage = "Invalid Phone")]
+        [Required(ErrorMessage = "Please enter a Phone Number")]
+        [RegularExpression(@"^\s*([\(]?)\[?\s*\d{3}\s*\]?[\)]?\s*[\-]?[\.]?\s*\d{3}\s*[\-]?[\.]?\s*\d{4}$", ErrorMessage = "Please enter a valid Phone Number")]
         public string Phone { get; set; }
 
         [StringLength(50)]
-        [Required(ErrorMessage = "Required")]
-        [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$", ErrorMessage = "Not a valid email")] //US ZIP  
+        [Required(ErrorMessage = "Please enter an E-mail address")]
+        [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$", ErrorMessage = "Please enter a valid E-mail Address")]
         public string Email { get; set; }
 
         
